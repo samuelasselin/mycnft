f8d2d13d83b9b23b7903f86b2d12730a6e7a0fdd
samuelasselin <c.samuelasselin@hotmail.com>
2022-02-20 19:54:21 -0500

    Prettier

--- next-env.d.ts
+++ next-env.d.ts
@@ -5,30 +5,31 @@
 // see https://nextjs.org/docs/basic-features/typescript for more information.
 
 import type {
-    NextComponentType,
-    NextPageContext,
-    NextLayoutComponentType,
-} from 'next';
-import type { AppProps } from 'next/app';
+  NextComponentType,
+  NextPageContext,
+  NextLayoutComponentType,
+} from "next";
+import type { AppProps } from "next/app";
 
-declare module 'next' {
-    type NextLayoutComponentType<P = {}> = NextComponentType<
-        NextPageContext,
-        any,
-        P
-        > & {
-        layout?: (page: ReactNode) => ReactNode;
-    };
+
+declare module "next" {
+  type NextLayoutComponentType<P = {}> = NextComponentType<
+    NextPageContext,
+    any,
+    P
+  > & {
+    layout?: (page: ReactNode) => ReactNode;
+  };
 }
 
-declare module 'next/app' {
-    type AppLayoutProps<P = {}> = AppProps & {
-        Component: NextLayoutComponentType;
-    };
+declare module "next/app" {
+  type AppLayoutProps<P = {}> = AppProps & {
+    Component: NextLayoutComponentType;
+  };
 }
 
 declare global {
-    interface Window {
-        cardano: any;
-    }
+  interface Window {
+    cardano: any;
+  }
 }
