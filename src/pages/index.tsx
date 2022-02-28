5ce254da06ca64000732183bbf43d4372c877eab
samuelasselin <c.samuelasselin@hotmail.com>
2022-02-27 21:27:40 -0500

    Me query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import { Hero } from "../components/Hero";
 import { AppBody } from "../layout/AppBody";
-
+import { useMeQuery } from "../generated/graphql";
+import { useWallet } from "../hooks/UseWallet";
 const Index = () => {
+  const { wallet } = useWallet();
+
+  if (!wallet.address) return <Hero title={"Loading.."} />;
+
+  if (wallet.address) {
+    const { data, loading, error } = useMeQuery({
+      variables: { address: wallet.address },
+    });
+
+    if (loading) return <h1>Loading..</h1>;
+    if (error) return <h1>{error}</h1>;
+
+    if (!data.me) {
+      return <Hero title={"Please choose a username"} />;
+    }
+  }
+
   return <Hero title={"Welcome to mycnft.io"} />;
 };
 
