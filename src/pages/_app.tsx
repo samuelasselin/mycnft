60ca6d5c85b0920e45e2367a08793214f93dab10
samuelasselin <c.samuelasselin@hotmail.com>
2022-04-14 19:48:34 -0400

    Remove graphql code gen appollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { AppLayoutProps } from "next/app";
 import { WalletContext } from "../hooks/UseWallet";
 import { useEffect, useState } from "react";
 import { namiWalletSignIn } from "../utils/NamiWallet";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppLayoutProps) {
   const Layout = Component.layout || (({ children }) => <>{children}</>);
@@ -12,11 +11,6 @@ function MyApp({ Component, pageProps }: AppLayoutProps) {
   const [wallet, setWallet] = useState({
     syncWallet: false,
     isInstalled: false,
-  });
-
-  const client = new ApolloClient({
-    uri: "http://localhost:3001/graphql",
-    cache: new InMemoryCache(),
   });
 
   useEffect(() => {
@@ -30,15 +24,13 @@ function MyApp({ Component, pageProps }: AppLayoutProps) {
   const value = { wallet, setWallet };
 
   return (
-    <ApolloProvider client={client}>
-      <WalletContext.Provider value={value}>
-        <ChakraProvider resetCSS theme={theme}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ChakraProvider>
-      </WalletContext.Provider>
-    </ApolloProvider>
+    <WalletContext.Provider value={value}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </WalletContext.Provider>
   );
 }
 
