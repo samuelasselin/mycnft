3e985795dacbd8581526635a31c847e483752a53
samuelasselin <c.samuelasselin@hotmail.com>
2022-05-20 16:35:56 -0400

    Deploy WIP

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  future: {
+    webpack5: false,
+  },
   webpack: (config) => {
-    const experiments = config.experiments || {};
-    config.experiments = { ...experiments, asyncWebAssembly: true };
+    config.output.webassemblyModuleFilename = "static/wasm/[modulehash].wasm";
     return config;
   },
 };
