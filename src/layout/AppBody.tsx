e19a59dacbadd1480939acae564a2e4b64f620af
samuelasselin <c.samuelasselin@hotmail.com>
2022-05-27 21:36:36 -0400

    Appbody component with head

--- src/layout/AppBody.tsx
+++ src/layout/AppBody.tsx
@@ -17,22 +17,11 @@ export const AppBody: React.FC<AppBodyProps> = ({ children, image }) => {
       <Head>
         <title>MyCnfts</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image:alt" content="MyCnfts" />
-        <meta name="og:image:width" content="650" />
-        <meta name="og:image:height" content="650" />
-        <meta
-          name="twitter:title"
-          content="Philippine Standard Geographic Code API"
-        />
-        <meta
-          name="twitter:description"
-          content="API used for listing all the region, province, city, municipality, barangay, and its data. Philippine Standard Geographic Codes (PSGC) data came from Philippine Statistics Authority."
-        />
-        <meta
-          name="twitter:image"
-          content="https://cf-ipfs.com/ipfs/QmfG2ry1y1stADHq9Ui5wZqc4LkALG2HCFPAb4kVuxDS8d"
-        />
+        <meta name="twitter:title" content="Nft title" />
+        <meta name="twitter:description" content="Nft description" />
+        <meta name="twitter:image" content={getAssetImageSource(image)} />
       </Head>
       <NavBar />
       <Container minHeight="100vh">
