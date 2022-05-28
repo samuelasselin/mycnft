ce95a5f66f05f6f47795e153f5d344d2df5c988d
samuelasselin <c.samuelasselin@hotmail.com>
2022-05-28 11:41:36 -0400

    Meta image with api route

--- src/pages/api/metaImage/index.tsx
+++ src/pages/api/metaImage/index.tsx
@@ -7,8 +7,7 @@ interface QueryParams {
 export default withOGImage<"query", QueryParams>({
   template: {
     // include HTML template here
-    html: ({ image }) =>
-      `<img height="650px" width="650px" alt='nft' src={image}/>`,
+    html: ({ image }) => `<h1>{image}</h1>`,
   },
   cacheControl: "public, max-age=604800, immutable",
   dev: {
