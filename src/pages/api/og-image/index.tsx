f60422d687ef902dd40135480c9b3f6f9825751d
samuelasselin <c.samuelasselin@hotmail.com>
2022-06-04 15:58:03 -0400

    Styling og-image

--- src/pages/api/og-image/index.tsx
+++ src/pages/api/og-image/index.tsx
@@ -4,12 +4,41 @@ interface QueryParams {
   src: string;
 }
 
+const style = `
+
+    html, body {
+        height: 100%;
+        margin:0;
+        padding:0;
+    }
+      
+    div {
+        position:relative;
+        height: 100%;
+        width:100%;
+    }
+
+    div img {
+        position:absolute;
+        top:0;
+        left:0;
+        right:0;
+        bottom:0;
+        margin:auto;
+    }
+`;
+
 export default withOGImage<"query", QueryParams>({
   template: {
     react: ({ src }) => (
       <html>
+        <head>
+          <style dangerouslySetInnerHTML={{ __html: style }} />
+        </head>
         <body>
-          <img src={src} width="350" height="350" alt="Nft" />
+          <div>
+            <img src={src} width="550" height="550" alt="Nft" />
+          </div>
         </body>
       </html>
     ),
